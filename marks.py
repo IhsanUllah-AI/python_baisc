d26ef43e9828802bfe454443b14c315bf1c3737a
unknown <ih161719838@gmail.com>
2024-12-22 20:29:47 +0500

    grade system

--- marks.py
+++ marks.py
@@ -1,7 +1,11 @@
 marks=int(input("enter marks ="))
 if marks>=90:
     print("A")
+elif marks>=80:
+    print("B")
 elif marks>=70:
     print("C")
+elif marks>=60:
+    print("D")
 else :
     print("fail")
